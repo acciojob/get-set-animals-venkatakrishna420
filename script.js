b3c8e9866edc2d7689fa6dee7abadd45a53b1291
KAKARLA VENKATA KRISHNA <kakarlavenkatakrishna9@gmail.com>
2025-09-01 16:29:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(spices){
+	this._spices=spices
+	}
+	get species(){
+		return this._spices
+	}
+	makeSound(){
+		console.log(`The ${this._spices} makes a sound`)
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	bark(){
+		console.log("woof")
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	purr(){
+		console.log("purr")
+	}
+}
+const myCat = new Cat("Siamese");
+myCat.makeSound();
+// Expected Output: The Siamese makes a sound
+
+myCat.purr();
+// Expected Output: purr
+
+// Creating an instance of Dog
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound();
+// Expected Output: The Golden Retriever makes a sound
+
+myDog.bark();
+// Expected Output: woof
 
 // Do not change the code below this line
 window.Animal = Animal;
